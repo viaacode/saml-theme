6ff51efb0716dae1d98a14fa95b14f3298296853
igloosarecool <bart.debunne@viaa.be>
2017-01-09 16:16:46 +0100

    Redesign 403 template.

--- themes/viaa/authorize/authorize_403.php
+++ themes/viaa/authorize/authorize_403.php
@@ -2,8 +2,7 @@
 
 // original file:  /var/www/simplesaml/modules/authorize/www/authorize_403.php
 
- header('HTTP/1.0 403 Forbidden');
-
+header('HTTP/1.0 403 Forbidden');
 
 if (!array_key_exists('StateId', $_REQUEST)) {
         throw new SimpleSAML_Error_BadRequest('Missing required StateId query parameter.');
@@ -17,26 +16,52 @@ if (!is_null($sid['url'])) {
         SimpleSAML_Utilities::checkURLAllowed($sid['url']);
 }
 
-
 ?>
+
 <html>
-    <head>
-        <title>VIAA SSO SERVER</title>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" type="text/css" href="<?php echo SimpleSAML_Module::getModuleURL('themeviaa/css/bootstrap.min.css') ?>">
-    </head>
-</head>
-<body class="logon_page">
+  <head>
+    <title>
+      Aanmelden bij VIAA - Toegang verboden
+    </title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="<?php echo SimpleSAML_Module::getModuleURL('themeviaa/css/furtive.min.css') ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo SimpleSAML_Module::getModuleURL('themeviaa/css/app.css') ?>">
+  </head>
+  <body>
     <div class="container">
-        <img src="http://viaa.be/assets/img/logo.jpg" style="width: 30%; height: 30%;" />
-
-<br>
-<h1>Access forbidden</h1>
-<br>
-You don't have the needed privileges to access this application. Please contact the administrator if you find this to be incorrect.
-<br><br>
-
-   </div>
- </body>
+      <div class="measure p2">
+        <div class="grd">
+          <div class="grd-row">
+            <div class="grd-row-col-1-5"></div>
+            <div class="grd-row-col-3-5 bg--off-white brdr--light-gray">
+              <div class="p2 py3 pb1 mb1 txt--center">
+                <img src="<?php echo SimpleSAML_Module::getModuleURL('themeviaa/img/Logo_Viaa_RGB_orange_kl.png') ?>" alt="Logo VIAA" style="width:120px">
+              </div>
+              <div class="alert alert-danger fnt--red mx2 bg--white" role="alert">
+                <h1>Geen toegang</h1>
+                <p>Je hebt geen toegang tot deze toepassing met jouw account.</p>
+              </div>
+              <div class="my2 small txt--center">
+                <p><a href="/module.php/core/authenticate.php?as=viaa-ldap&logout">Andere account proberen?</a></p>
+              </div>
+              <div class="txt--center mb1">
+                <p><code class="small fnt--mid-gray">© 2017 / <a href="http://www.viaa.be">www.viaa.be</a></code>
+              </div>
+            </div>
+            <div class="grd-row-col-1-5"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    <!-- Start of viaa Zendesk Widget script -->
+    <script>/*<![CDATA[*/window.zEmbed||function(e,t){var n,o,d,i,s,a=[],r=document.createElement("iframe");window.zEmbed=function(){a.push(arguments)},window.zE=window.zE||window.zEmbed,r.src="javascript:false",r.title="",r.role="presentation",(r.frameElement||r).style.cssText="display: none",d=document.getElementsByTagName("script"),d=d[d.length-1],d.parentNode.insertBefore(r,d),i=r.contentWindow,s=i.document;try{o=s}catch(c){n=document.domain,r.src='javascript:var d=document.open();d.domain="'+n+'";void(0);',o=s}o.open()._l=function(){var o=this.createElement("script");n&&(this.domain=n),o.id="js-iframe-async",o.src=e,this.t=+new Date,this.zendeskHost=t,this.zEQueue=a,this.body.appendChild(o)},o.write('<body onload="document._l();">'),o.close()}("https://assets.zendesk.com/embeddable_framework/main.js","viaa.zendesk.com");/*]]>*/</script>
+    <script>
+    zE(function() {
+    zE.setLocale('nl');
+    });
+    </script>
+    <!-- End of viaa Zendesk Widget script -->
+  </body>
 </html>
