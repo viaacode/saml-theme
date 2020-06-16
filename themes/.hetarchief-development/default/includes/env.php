f078ca609a7c00113212238a16feb77f2228dd31
root Puppet-managed User <root@dg-tst-idp-a0.dg.viaa.be>
2020-06-16 14:52:18 +0200

    DEV-939 Added .dotfiles for themes.

---
+++ themes/.hetarchief-development/default/includes/env.php
@@ -0,0 +1,20 @@
+<?php
+$env = "development";
+switch ($env) {
+  case "integration":
+    $prefix= "-tst";
+  break;
+  case "development":
+    $prefix= "-tst";
+  break;
+  case "staging":
+    $prefix= "-qas";
+  break;
+  case "production":
+    $prefix= "";
+  break;
+}
+$this->data['meemoo_environment'] = $prefix;
+
+echo "<!--". ($env) .": ".$prefix." -->";
+?>
