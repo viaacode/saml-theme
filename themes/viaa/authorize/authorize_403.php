b5151cc88a879ee588c3f8e9479ad57c3922d7c2
Bart Debunne <igloosarecool@users.noreply.github.com>
2020-06-19 16:22:42 +0200

    Rebrand 403 unauthorized

--- themes/viaa/authorize/authorize_403.php
+++ themes/viaa/authorize/authorize_403.php
@@ -21,12 +21,14 @@ if (!is_null($sid['url'])) {
 <html>
   <head>
     <title>
-      Aanmelden bij VIAA - Toegang verboden
+      Aanmelden bij meemoo - Toegang verboden
     </title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="<?php echo SimpleSAML\Module::getModuleURL('themeviaa/css/furtive.min.css') ?>">
-    <link rel="stylesheet" type="text/css" href="<?php echo SimpleSAML\Module::getModuleURL('themeviaa/css/app.css') ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo SimpleSAML\Module::getModuleURL('themeviaa/css/app-ab44afa1-f3b3-4249-b294-1bb638329f5a.css') ?>">
+    <link rel="icon" type="image/png" href="<?php echo SimpleSAML\Module::getModuleURL('themeviaa/img/meemoo_favicon-306d29737c36438ecc5387780be080cc0516d0e71774edb3a47a62ef5a7a00cd.png') ?>" sizes="32x32" />
+    <link rel="icon" type="image/png" href="<?php echo SimpleSAML\Module::getModuleURL('themeviaa/img/meemoo_favicon-306d29737c36438ecc5387780be080cc0516d0e71774edb3a47a62ef5a7a00cd.png') ?>" sizes="16x16" />
   </head>
   <body>
     <div class="container">
@@ -36,7 +38,7 @@ if (!is_null($sid['url'])) {
             <div class="grd-row-col-1-5"></div>
             <div class="grd-row-col-3-5 bg--off-white brdr--light-gray">
               <div class="p2 py3 pb1 mb1 txt--center">
-                <img src="<?php echo SimpleSAML\Module::getModuleURL('themeviaa/img/Logo_Viaa_RGB_orange_kl.png') ?>" alt="Logo VIAA" style="width:120px">
+                <img src="<?php echo SimpleSAML\Module::getModuleURL('themeviaa/img/30c6269d-8c77-4be1-987b-260620de88b0.png') ?>" alt="Logo meemoo" style="width:75%">
               </div>
 	      <div class="alert alert-danger fnt--red mx2 bg--white" role="alert">
                  <?php
@@ -47,10 +49,10 @@ if (!is_null($sid['url'])) {
 		 }?>
 	      </div>
               <div class="my2 small txt--center">
-                <p>Vragen? Contacteer <a href="mailto:support@viaa.be?subject=Toegang%20met%20VIAA-account">support@viaa.be</a></p>
+                <p>Vragen? Contacteer <a href="mailto:support@viaa.be?subject=Toegang%20met%20VIAA-account">support@meemoo.be</a></p>
               </div>
               <div class="txt--center mb1">
-                <p><code class="small fnt--mid-gray">© 2017 / <a href="http://www.viaa.be">www.viaa.be</a></code>
+                <span class="small fnt--mid-gray">© 2020 / <a href="http://www.viaa.be">www.meemoo.be</a></span>
               </div>
             </div>
             <div class="grd-row-col-1-5"></div>
