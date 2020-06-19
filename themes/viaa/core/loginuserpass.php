a8b05606a312761eab833d81d75fd7bc22f4be98
Bart Debunne <igloosarecool@users.noreply.github.com>
2020-06-19 16:27:38 +0200

    replace viaa by meemoo in title

--- themes/viaa/core/loginuserpass.php
+++ themes/viaa/core/loginuserpass.php
@@ -1,7 +1,7 @@
 <html>
 	<head>
 		<title>
-			Aanmelden bij VIAA
+			Aanmelden bij meemoo
 		</title>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
