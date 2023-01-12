eb583beee24a20a9011c671c34469babae3505bc
Bart Debunne <bart.debunne@gmail.com>
2023-01-12 13:58:41 +0100

    LM-59 update theme files

---
+++ themes/.hetarchief-integration/default/includes/_form-header.php
@@ -0,0 +1,13 @@
+<div class="o-container-vertical" style="margin-top:2em;">
+    <div class="o-container o-container--small">
+      <div class="u-spacer-bottom-l">
+        <div class="o-flex o-flex--align-baseline o-flex--justify-between">
+          <div class="">
+            <img src="<?php echo SimpleSAML\Module::getModuleURL('themeviaa/img/logo-meemoo.svg')?>" height="90" alt="Logo meemoo - Vlaams Instituut voor het Archief" title="Logo meemoo" />
+          </div>
+          <div class="">
+            <img src="<?php echo SimpleSAML\Module::getModuleURL('themeviaa/img/logo-hetarchief.svg')?>" height="40" alt="Logo Het Archief - Een initiatief van meemoo" title="Logo Het Archief" />
+          </div>
+        </div>
+      </div>
+      <hr class="c-hr">
