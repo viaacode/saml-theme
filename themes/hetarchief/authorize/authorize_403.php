962e128e9ced4eee613f8fb24a2cb172a1b114cd
Bart Debunne <bart.debunne@gmail.com>
2023-01-12 13:01:33 +0100

    LM-59 Add new header to 403 page

--- themes/hetarchief/authorize/authorize_403.php
+++ themes/hetarchief/authorize/authorize_403.php
@@ -69,12 +69,17 @@ $message .= "</p>";
 </head>
 
 <body>
-  <div class="o-container-vertical">
+<div class="o-container-vertical" style="margin-top:2em;">
     <div class="o-container o-container--small">
       <div class="u-spacer-bottom-l">
-        <h1 class="c-brand c-brand--large">
-          <img src="<?php echo SimpleSAML\Module::getModuleURL('themeviaa/img/logo-algemeen.svg')?>" alt="Het Archief">
-        </h1>
+        <div class="o-flex o-flex--align-baseline o-flex--justify-between">
+          <div class="">
+            <img src="<?php echo SimpleSAML\Module::getModuleURL('themeviaa/img/logo-meemoo.svg')?>" height="90" alt="Logo meemoo - Vlaams Instituut voor het Archief" title="Logo meemoo" />
+          </div>
+          <div class="">
+            <img src="<?php echo SimpleSAML\Module::getModuleURL('themeviaa/img/logo-hetarchief.svg')?>" height="40" alt="Logo Het Archief - Een initiatief van meemoo" title="Logo Het Archief" />
+          </div>
+        </div>
       </div>
       <hr class="c-hr">
       <h3 class="c-h2">Toegang geweigerd</h3>
