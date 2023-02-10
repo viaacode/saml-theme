d954e6508e65757417575556746cb34e70626089
Bart Debunne <bart.debunne@gmail.com>
2023-02-10 14:50:08 +0100

    LM-110 Move logo size to css

--- themes/hetarchief/core/loginuserpass.php
+++ themes/hetarchief/core/loginuserpass.php
@@ -49,11 +49,11 @@ if (!is_null($sid['url'])) {
     <div class="o-container o-container--small">
       <div class="u-spacer-bottom-l">
         <div class="o-flex o-flex--align-baseline o-flex--justify-between">
-          <div class="">
-            <img src="<?php echo SimpleSAML\Module::getModuleURL('themeviaa/img/logo-meemoo.svg')?>" height="90" alt="Logo meemoo - Vlaams Instituut voor het Archief" title="Logo meemoo" />
+          <div>
+            <img src="<?php echo SimpleSAML\Module::getModuleURL('themeviaa/img/logo-meemoo.svg')?>" alt="Logo meemoo - Vlaams Instituut voor het Archief" title="Logo meemoo" class="logo--meemoo" />
           </div>
-          <div class="">
-            <img src="<?php echo SimpleSAML\Module::getModuleURL('themeviaa/img/logo-hetarchief.svg')?>" height="40" alt="Logo Het Archief - Een initiatief van meemoo" title="Logo Het Archief" />
+          <div>
+            <img src="<?php echo SimpleSAML\Module::getModuleURL('themeviaa/img/logo-hetarchief.svg')?>" alt="Logo Het Archief - Een initiatief van meemoo" title="Logo Het Archief" class="logo--hetarchief" />
           </div>
         </div>
       </div>
